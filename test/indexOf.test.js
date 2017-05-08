feb7ab7ef57246c348992490a8d6c783282b3830
Corey <cry@Coreys-Air.attlocal.net>
2017-05-07 23:32:45 -0700

    Added tests ti last and indexOf

--- test/indexOf.test.js
+++ test/indexOf.test.js
@@ -26,4 +26,9 @@ describe('indexOf()', () => {
     expect(_.indexOf(arr, 'bar', 2)).toBe(3);
   });
 
+  it('returns -1 for a value in the array but after the offeset', () => {
+    const arr = ['foo', 'bar', 'baz', 'bar', 'bar'];
+    expect(_.indexOf(arr, 'foo', 1)).toBe(-1);
+  });
+
 });
--- test/last.test.js
+++ test/last.test.js
@@ -12,4 +12,8 @@ describe('last()', () => {
   it('returns the whole array if you ask for more elements than it has', () => {
     expect(_.last(['a', 'b', 'c'], 5)).toEqual(['a', 'b', 'c']);
   });
+
+  it('returns the last 4 elements of the array', () => {
+    expect(_.last(['1', '2', '3', '4', '5'], 4)).toEqual(['2', '3', '4', '5']);
+  });
 });
