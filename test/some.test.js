0a5c1e774648bc8d975599a65d30c3c3f0145081
Corey <cry@Coreys-Air.attlocal.net>
2017-05-08 08:09:59 -0700

    Added test to 'some'

--- test/some.test.js
+++ test/some.test.js
@@ -10,5 +10,13 @@ describe('some()', () => {
     const nums = [2, 4, 6, 8];
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
-
+  it('returns true if any value in the object is rabbit', () => {
+    const animals = {
+      'Franklin': 'turtle',
+      'Peter': 'rabbit',
+      'Arthur': 'dog'
+    }
+    console.log()
+    expect(_.some(animals, species => species === 'rabbit')).toBe(true);
+  });
 });
